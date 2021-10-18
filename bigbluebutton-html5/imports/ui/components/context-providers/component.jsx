35df858952721c1de9726d36934256cc424e906b
Ramón Souza <contato@ramonsouza.com>
2021-10-18 13:01:19 +0000

    add hot reload

--- bigbluebutton-html5/imports/ui/components/context-providers/component.jsx
+++ bigbluebutton-html5/imports/ui/components/context-providers/component.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
+import { hot } from 'react-hot-loader';
 import { ChatContextProvider } from '/imports/ui/components/components-data/chat-context/context';
 import { UsersContextProvider } from '/imports/ui/components/components-data/users-context/context';
 import { GroupChatContextProvider } from '/imports/ui/components/components-data/group-chat-context/context';
-
 
 const providersList = [
   ChatContextProvider,
@@ -10,9 +10,10 @@ const providersList = [
   UsersContextProvider,
 ];
 
-const ContextProvidersComponent = props => providersList.reduce((acc, Component) => (
+const ContextProvidersComponent = (props) => providersList.reduce((acc, Component) => (
   <Component>
     {acc}
-  </Component>), props.children);
+  </Component>
+), props.children);
 
-export default ContextProvidersComponent;
+export default hot(module)(ContextProvidersComponent);
