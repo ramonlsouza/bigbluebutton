e3f53523ec2230cea79d033a26b763d81ae806a7
Ramón Souza <contato@ramonsouza.com>
2021-10-18 13:00:45 +0000

    remove css-modules lib and add webpack files

---
+++ bigbluebutton-html5/webpack.config.js
@@ -0,0 +1,67 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const meteorExternals = require('webpack-meteor-externals');
+
+const clientConfig = {
+  entry: './client/main.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['react-hot-loader/babel'],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: {
+                localIdentName: '[local]__[hash:base64:5]',
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  devServer: {
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/main.html',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'bundle.js',
+  },
+  externals: [
+    meteorExternals(),
+  ],
+};
+const serverConfig = {
+  entry: [
+    './server/main.js',
+  ],
+  target: 'node',
+  externals: [
+    meteorExternals(),
+  ],
+};
+module.exports = [clientConfig, serverConfig];
