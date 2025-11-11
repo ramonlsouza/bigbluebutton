4c0d63d46b7bf19fb1b25df2d2a06499a2608286
Ramón Souza <contato@ramonsouza.com>
2025-11-11 15:24:53 -0300

    remove showPushLayoutButton and showPushLayoutToggle settings

--- bigbluebutton-html5/imports/ui/Types/meetingClientSettings.ts
+++ bigbluebutton-html5/imports/ui/Types/meetingClientSettings.ts
@@ -601,8 +601,6 @@ export interface Notes {
 export interface Layout {
   hidePresentationOnJoin: boolean
   showParticipantsOnLogin: boolean
-  showPushLayoutButton: boolean
-  showPushLayoutToggle: boolean
   showScreenshareQuickSwapButton: boolean
   showLeaveSessionLabel: boolean
 }
