c9656985a64a27143bd227068aa56690d0993fc2
Ramón Souza <contato@ramonsouza.com>
2025-11-17 08:45:15 -0300

    sync presentation state for viewer

--- bigbluebutton-html5/imports/ui/components/layout/push-layout/pushLayoutEngine.jsx
+++ bigbluebutton-html5/imports/ui/components/layout/push-layout/pushLayoutEngine.jsx
@@ -226,6 +226,7 @@ const PushLayoutEngine = (props) => {
           type: ACTIONS.SET_PRESENTATION_IS_OPEN,
           value: meetingPresentationIsOpen,
         });
+        Session.setItem('presentationLastState', meetingPresentationIsOpen);
       }
     };
 
